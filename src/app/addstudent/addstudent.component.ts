8f0fb562e10c24fb93316902ec9abf3fe51a0ebc
Riya505 <riyapk505@gmail.com>
2022-10-14 01:09:23 +0530

    new

--- src/app/addstudent/addstudent.component.ts
+++ src/app/addstudent/addstudent.component.ts
@@ -9,6 +9,35 @@ export class AddstudentComponent implements OnInit {
 
   constructor() { }
 
+  name=""
+  rollno=""
+  college=""
+  admissionNo=""
+  parentName=""
+  dateOfBirth=""
+  address=""
+  mobile=""
+  parentMobile=""
+  email=""
+  bloodGroup=""
+
+  readValues=()=>{
+    let data={
+      "name":this.name,
+      "rollno":this.rollno,
+      "college":this.college,
+      "admissionNo":this.admissionNo,
+      "parentName":this.parentName,
+      "dateOfBirth":this.dateOfBirth,
+      "address":this.address,
+      "mobile":this.mobile,
+      "parentMobile":this.parentMobile,
+      "email":this.email,
+      "bloodGroup":this.bloodGroup
+    }
+    console.log(data)
+  }
+
   ngOnInit(): void {
   }
 
